dd50714b2b261aa363d1e10321f10b12de4b066c
OwisHassan <aohs999@gmail.com>
2019-01-25 21:33:56 +0300

    MVR

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float Speed;
+    void Start ()
+    {
+		
+	}
+
+
+    void Update ()
+    {
+        float X = Input.GetAxis("Vertical");
+        transform.Translate(Speed * X * Time.deltaTime,0,0);
+	}
+}
